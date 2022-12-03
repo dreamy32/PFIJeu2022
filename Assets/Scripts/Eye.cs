2e41270cb9a36b7d57d08fdd0a7e33a1e28b3016
Benjamin Piché <ben.piche@outlook.com>
2022-12-02 19:56:11 -0500

    oui
    non

---
+++ Assets/Scripts/Eye.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eye : MonoBehaviour
+{
+    [SerializeField] Transform playerPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.LookAt(playerPos);
+        transform.rotation *= Quaternion.FromToRotation(Vector3.left, Vector3.forward);
+
+    }
+    
+    
+
+  
+}
