1b77a234750404ed43e7144b4385c77055b78b06
Benjamin Piché <ben.piche@outlook.com>
2022-12-09 15:34:10 -0500

    SmallDoor
    oui

--- Assets/Scripts/SmallDoorComponent.cs
+++ Assets/Scripts/SmallDoorComponent.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class SmallDoorComponent : InteractableObject
 {
     private Animator _animator;
-    private static readonly int OpenTrigger = Animator.StringToHash("Open");
+    bool isOpen;
+    private AudioSource audio;
 
+    [SerializeField] AudioClip openingSound;
+    [SerializeField] AudioClip closingSound;
+
+    //private static readonly int OpenTrigger = Animator.StringToHash("Open");
     protected override void Awake()
     {
         //InteractableObject attributes
         base.Awake();
-        SingleUsage = true; //Éviter de call l'animator pour rien
+        SingleUsage = false; //Éviter de call l'animator pour rien
+        isOpen = false;
         _animator = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
     }
-
+    private void PlaySound(AudioClip clip)
+    {
+        audio.clip = clip;
+        audio.Play();
+    }
     protected override void OnInteract()
     {
-        Debug.Log("oui");
-        _animator.SetTrigger("Open");
+        if (!isOpen)
+        {
+            PlaySound(openingSound);
+
+            _animator.SetTrigger("Open");
+            _animator.ResetTrigger("Close");
+            isOpen = true;
+        }
+        else
+        {
+            PlaySound(closingSound);
+
+            _animator.SetTrigger("Close");
+            _animator.ResetTrigger("Open");
+            isOpen = false;
+        }
     }
 }
