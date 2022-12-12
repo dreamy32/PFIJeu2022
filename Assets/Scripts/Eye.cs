dc15a25efaa65d2d84a257e4722d84fe4b8eebef
tomcool2002 <32969040+tomcool2002@users.noreply.github.com>
2022-12-12 12:08:10 -0500

    eye

--- Assets/Scripts/Eye.cs
+++ Assets/Scripts/Eye.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class Eye : MonoBehaviour
 {
     [SerializeField] private Transform playerPos;
+    [SerializeField] private MonsterBehaviourTreeComponent monster;
+    [SerializeField] private float distance = 10f;
 
     // Update is called once per frame
     void Update()
     {
         transform.LookAt(playerPos);
         transform.rotation *= Quaternion.FromToRotation(Vector3.left, Vector3.forward);
+
+        if(Vector3.Distance(transform.position,playerPos.position) <= distance)
+        {
+            monster.SwitchWing(playerPos);
+        }
 
     }
     
