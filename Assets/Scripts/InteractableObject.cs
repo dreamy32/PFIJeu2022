bde1f9ee3630d630d6d4d6814ca957be70fc1986
Dr. Seek <75817900+dreamy32@users.noreply.github.com>
2022-12-11 23:36:19 -0500

    Fix onDisable pour molson

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
@@ -69,6 +70,13 @@ public abstract class InteractableObject : MonoBehaviour
                 ToggleInfo(false);
             }
         };
+    }
+
+    protected void OnDisable()
+    {
+        _isTriggered = false;
+        _canInteract = false;
+        ToggleInfo(false);
     }
 
     protected virtual void Start()
